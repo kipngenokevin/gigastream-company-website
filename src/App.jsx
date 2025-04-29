f7ea1040ca76fb36b77d932ed990b526aa0ae469
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2025-04-29 15:40:59 +0300

    Adds meta description and react-helmet for SEO
    Adds meta description to improve SEO for the website.
    Implements React Helmet for dynamic title and meta tags.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import MainSection from './MainSection'
 import Footer from './Footer'
 import Hero from './Hero'
+import { Helmet } from 'react-helmet'
 
 function App() {
 
   return (
-    <>
-      <Hero />
-      <MainSection />
-      <Footer />
-    </>
-  )
+		<>
+			<Helmet>
+				<title>Gigastream Media</title>
+				<meta
+					name='description'
+					content='Gigastream Media offers innovative branding and advertising solutions to engage audiences and drive growth.'
+				/>
+			</Helmet>
+			<Hero />
+			<MainSection />
+			<Footer />
+		</>
+  );
 }
 
 export default App
